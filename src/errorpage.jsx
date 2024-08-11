6dc6684cd800b801b70bba899d36e5c15b41f110
= <bottenbergdave@gmail.com>
2024-08-11 19:07:45 -0400

    began formatting error page

--- src/errorpage.jsx
+++ src/errorpage.jsx
@@ -1,13 +1,15 @@
 import {useRouteError} from "react-router-dom"
+import {CircleX} from 'lucide-react'
 
 function ErrorPage() {
     const error = useRouteError();
     console.log(error);
 
     return (
-        <div>
-            <h1>Oops. Theres an error</h1>
-            <p>sorry an unexpected error has occured</p>
+        <div className="error">
+            <h1>Oops. There&apos;s an error...</h1>
+            <CircleX className="errorLogo"/>
+            <p>Sorry, an unexpected error has occured...</p>
         </div>
     )
 }
